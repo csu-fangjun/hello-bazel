3d93ac517aa48a6d94bef08722be675b3fe71c6b
csu-fangjun <3318684+csu-fangjun@users.noreply.github.com>
2019-01-01 15:52:52 +0800

    Update scripts.sh

--- travis/scripts.sh
+++ travis/scripts.sh
@@ -9,8 +9,11 @@ if [[ x"$1" == x"linux" ]]; then
   export PATH=${HOME}/software/qemu/bin:${PATH}
   export LD_LIBRARY_PATH=$HOME/software/linaro/aarch64-linux-gnu/libc/lib:${LD_LIBRARY_PATH}
   bazel build --config=aarch64 //...
-  bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_arm
-  bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_bazel
-  bazel test --run_under=qemu-aarch64 --config=aarch64 //src:all
+  qemu-aarch64 bazel-bin/src/hello_arm
+  qemu-aarch64 bazel-bin/src/hello_bazel
+  qemu-aarch64 bazel-bin/src/hello_test
+  # bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_arm
+  # bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_bazel
+  # bazel test --run_under=qemu-aarch64 --config=aarch64 //src:all
 fi
 
