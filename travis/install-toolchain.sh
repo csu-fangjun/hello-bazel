a364d6bf4837ef8643fdd46447b63e289b1fc403
Fangjun Kuang <fangjun.kuang@gmail.com>
2019-01-01 12:53:12 +0800

    ci for aarch64.

---
+++ travis/install-toolchain.sh
@@ -0,0 +1,17 @@
+
+mkdir $HOME/software
+cd $HOME/software
+
+wget https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/aarch64-linux-gnu/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
+tar xf gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
+mv gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu linaro
+
+# install qemu from source
+sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+git clone --depth 1 git://git.qemu-project.org/qemu.git
+cd qemu
+./configure --prefix=$HOME/software/qemu --target-list=aarch64-linux-user
+make -s -j4 > /dev/null
+make install > /dev/null
+
+qemu-aarch64 --version
--- travis/scripts.sh
+++ travis/scripts.sh
@@ -3,3 +3,14 @@
 bazel build //...
 bazel test //...
 
+if [[ x"$1" == x"linux" ]]; then
+  echo "------build aarch64------"
+
+  export PATH=${HOME}/software/qemu/bin:${PATH}
+  export LD_LIBRARY_PATH=$HOME/software/linaro/aarch64-linux-gnu/libc/lib:${LD_LIBRARY_PATH}
+  bazel build --config=aarch64 //...
+  bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_arm
+  bazel run --run_under=qemu-aarch64 --config=aarch64 //src:hello_bazel
+  bazel test --run_under=qemu-aarch64 --config=aarch64 //src:all
+fi
+
