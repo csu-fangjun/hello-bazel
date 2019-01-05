a678eb97260208d46c6feb42bddcdded0c258851
Fangjun Kuang <fangjun.kuang@gmail.com>
2019-01-05 11:43:35 +0800

    ci for aarch64.

--- travis/install-toolchain.sh
+++ travis/install-toolchain.sh
@@ -5,6 +5,7 @@ cd $HOME/software
 wget https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/aarch64-linux-gnu/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
 tar xf gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
 mv gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu linaro
+sudo cp linaro/aarch64-linux-gnu/libc/lib/ld-linux-aarch64.so.1 /lib/
 
 # install qemu from source
 sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
